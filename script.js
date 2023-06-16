905e574f9cf024eed05b41e68ad014f1a34f6082
Agustinus <lagustinus211@gmail.com>
2023-06-16 10:53:16 +0700

    passs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/webface/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/webface/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/webface/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/webface/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -32,52 +32,57 @@ video.addEventListener('play', () => {
       const emotions = detections[0].expressions;
       const sortedEmotions = Object.entries(emotions).sort((a, b) => b[1] - a[1]);
       const dominantEmotion = sortedEmotions[0][0];
-      emotionprt(dominantEmotion);
+      polym(dominantEmotion);
     }
   }, 5000)
 })
 
+function polym(params) {
+
+  if (params != "neutral") {
+    // dont do anything
+  } else {
+    const API_KEY = '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';
+    const API_URL = 'https://api.openai.com/v1/chat/completions';
+    const qw= "I FERREN Have Feal like "+params+" Please talk to me with questions asking about my emotions";
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${API_KEY}`
+    };
+
+    const requestData = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'user', content: qw },
+      ],
+      temperature: 0.7
+    };
+
+    fetch(API_URL, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(requestData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        console.log(data.choices[0].message.content);
+        emotionprt(data.choices[0].message.content);
+
+      })
+      .catch(error => {
+        console.error('Terjadi kesalahan:', error);
+      });
+  }
+}
+
 function emotionprt(emotions) {
   console.log(emotions);
   if ('speechSynthesis' in window) {
-    const message = new SpeechSynthesisUtterance(`${emotions}`);
+    const message = new SpeechSynthesisUtterance(`Ferren Andrea. ${emotions} , and you mush to know, Agustinus allways love you`);
     speechSynthesis.speak(message);
   } else {
     console.log('Text-to-speech is not supported in this browser');
   }
-  //request pertanyaan ke https://openai.com/
 
 }
-
-
-
-const API_ENDPOINT = 'https://api.openai.com/v1/chat/completion';
-
-function askQuestion(question) {
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': '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' // Replace with your actual API key
-    },
-    body: JSON.stringify({
-      prompt: question,
-      max_tokens: 100
-    })
-  };
-
-  fetch(API_ENDPOINT, requestOptions)
-    .then(response => response.json())
-    .then(data => handleResponse(data))
-    .catch(error => console.error('Error:', error));
-}
-
-function handleResponse(data) {
-  // Handle the response from the OpenAI API here
-  console.log(data.choices[0].text);
-  // You can use the generated text or perform further processing as needed
-}
-
-// Example usage
-const question = "What is the capital of France?";
-askQuestion(question);
