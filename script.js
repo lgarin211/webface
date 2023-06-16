8858150102310a6f8389e3a9a3ef9e8acebfdb87
Agustinus <lagustinus211@gmail.com>
2023-06-16 10:54:11 +0700

    pass

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://lgarin211.github.io/webface/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://lgarin211.github.io/webface/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://lgarin211.github.io/webface/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://lgarin211.github.io/webface/models')
 ]).then(startVideo)
 
 function startVideo() {
