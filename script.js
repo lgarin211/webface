fb05befa9652d483e85d43e36a4e2518014125e0
Agustinus <lagustinus211@gmail.com>
2023-06-16 10:24:07 +0700

    pass

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/webface/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/webface/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/webface/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/webface/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -27,5 +27,57 @@ video.addEventListener('play', () => {
     faceapi.draw.drawDetections(canvas, resizedDetections)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+
+    if (detections.length > 0) {
+      const emotions = detections[0].expressions;
+      const sortedEmotions = Object.entries(emotions).sort((a, b) => b[1] - a[1]);
+      const dominantEmotion = sortedEmotions[0][0];
+      emotionprt(dominantEmotion);
+    }
+  }, 5000)
 })
+
+function emotionprt(emotions) {
+  console.log(emotions);
+  if ('speechSynthesis' in window) {
+    const message = new SpeechSynthesisUtterance(`${emotions}`);
+    speechSynthesis.speak(message);
+  } else {
+    console.log('Text-to-speech is not supported in this browser');
+  }
+  //request pertanyaan ke https://openai.com/
+
+}
+
+
+
+const API_ENDPOINT = 'https://api.openai.com/v1/chat/completion';
+
+function askQuestion(question) {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': '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' // Replace with your actual API key
+    },
+    body: JSON.stringify({
+      prompt: question,
+      max_tokens: 100
+    })
+  };
+
+  fetch(API_ENDPOINT, requestOptions)
+    .then(response => response.json())
+    .then(data => handleResponse(data))
+    .catch(error => console.error('Error:', error));
+}
+
+function handleResponse(data) {
+  // Handle the response from the OpenAI API here
+  console.log(data.choices[0].text);
+  // You can use the generated text or perform further processing as needed
+}
+
+// Example usage
+const question = "What is the capital of France?";
+askQuestion(question);
